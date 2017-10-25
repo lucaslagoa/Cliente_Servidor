6ee7f01b3e100d81daa6d941b76cb5b4bc38fa4d
Lucas Lagoa <lucaslagoanogueira@hotmail.com>
2017-10-25 14:20:59 -0200

    Cliente - falta arrumar a string | Servidor - falta arrumar o magico la pra imprimir todos arquivos

---
+++ Cliente_Servidor/src/Cliente.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+	static Scanner s;
+	static String navegador;
+	static String url;
+	static String door;
+	private static FileWriter arquivo;
+	private static Scanner sc;
+	private static Socket socket;
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("navegador http://urldesejada Porta");
+		s = new Scanner(System.in);
+		String re = s.nextLine();
+		System.out.println(re);
+		String[] parts = re.split(" ");
+		if (parts.length == 3) {
+			navegador = parts[0];
+			url = parts[1];
+			door = parts[2];
+		} else if (parts.length == 2) {
+			navegador = parts[0];
+			url = parts[1];
+			door = "80";
+		} else {
+			System.out.println("O ESPACO EM BRANCO");
+		}
+		int porta = Integer.parseInt(door);
+		socket = new Socket(url, porta);
+		// verifica se esta conectado
+		if (socket.isConnected()) {
+			// imprime o endereço de IP do servidor
+			System.out.println("Conectado a " + socket.getInetAddress());
+			/*
+			 * veja que a requisição termina com \r\n que equivale a <CR><LF>
+			 * para encerar a requisição tem uma linha em branco
+			 */
+			String requisicao = "" + "GET /~fls/redes/tp1.txt HTTP/1.1\r\n" + "Host: www.dcomp.ufsj.edu.br\r\n" + "\r\n";
+			// OutputStream para enviar a requisição
+			//PrintStream envioServ = new PrintStream(socket.getOutputStream());
+
+			OutputStream envioServ = socket.getOutputStream();
+			// temos que mandar a requisição no formato de vetor de bytes
+			byte[] b = requisicao.getBytes();
+			
+			// escreve o vetor de bytes no "recurso" de envio
+			envioServ.write(b);
+			System.out.println(envioServ);
+			// marca a finalização da escrita
+			envioServ.flush();
+			
+			sc = new Scanner(socket.getInputStream());
+			arquivo = new FileWriter(new File("tp1.txt"));
+		
+
+			while (sc.hasNext()) {
+				// imprime uma linha da resposta
+				String line = sc.nextLine();
+				System.out.println(line);
+				arquivo.write(line);
+				arquivo.write("\n");
+				
+			}
+			arquivo.flush();
+			arquivo.close();
+		}
+
+	}
+
+}
---
+++ Cliente_Servidor/src/Servidor.java
@@ -0,0 +1,152 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Servidor implements Runnable {
+
+	static ObjectOutputStream saida;
+	static OutputStream os;
+	static int porta = 12354;
+	static String navegador;
+	static String url;
+	static String door;
+	static PrintStream mensagem;
+	static Socket servidor;
+	static ServerSocket sservidor;
+	static BufferedWriter responseWriter;
+	
+	
+	private static final String OUTPUT_NOT_FOUND = "<html><head><title>Not Found</title></head><body><p>Resource Not Found!!</p></body></html>";
+	private static final String OUTPUT_HEADERS_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n" +
+	    "Content-Type: text/html\r\n" + 
+	    "Content-Length: ";
+	private static final String OUTPUT_END_OF_HEADERS = "\r\n\r\n";	
+	Servidor(Socket servidor) {
+	      this.servidor = servidor;
+	}
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		sservidor = new ServerSocket(porta);
+		System.out.println("Servidor está rodando na porta - " + porta + "\n");
+		
+		 while (true) {
+	         Socket servidor = sservidor.accept();
+	         new Thread(new Servidor(servidor)).start();
+	      }
+		 
+	}
+	
+	public void run() {
+		System.out.println("Nova conexão com o servidor " + servidor.getInetAddress().getHostAddress() + "\n");
+
+		try {
+			responseWriter = new BufferedWriter(new OutputStreamWriter(
+				        new BufferedOutputStream(servidor.getOutputStream()), "UTF-8"));
+			//responseWriter.write(OUTPUT_HEADERS + OUTPUT.length() + OUTPUT_END_OF_HEADERS + OUTPUT);
+
+			Scanner s = new Scanner(servidor.getInputStream());
+//
+			while (s.hasNextLine()) {
+				String requisicao = s.nextLine();
+				System.out.println(requisicao);
+				String[] parts = requisicao.split(" ");
+				if (parts.length == 3) {
+					navegador = parts[0];
+					url = parts[1];
+					door = parts[2];
+				} else if (parts.length == 2) {
+					navegador = parts[0];
+					url = parts[1];
+					door = "8080";
+				} else {
+					System.out.println("O ESPACO EM BRANCO");
+				}
+				break;
+			}
+			GET();
+			responseWriter.flush();
+			responseWriter.close();
+			servidor.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	static public void GET() throws IOException, ClassNotFoundException {
+
+		String nomeArquivo = "/home/lucas/workspace/Servidor/src/" + url;
+		
+
+		File file = new File(nomeArquivo);
+//		saida.write("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 0\r\n\r\n".getBytes("ASCII"));
+//		saida.flush();
+//		os.write(new String("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n").getBytes());
+//		os.flush();
+
+		
+		if (file.exists()) {
+			OutputStream stream = servidor.getOutputStream();
+			stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/pdf\r\n".getBytes());
+//			responseWriter.write(msg);
+//			saida.flush();
+
+			FileInputStream fis = new FileInputStream(file);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+ 
+//			String contentLength = "Content-Length: " + String.valueOf(file.length()) + "\r\n\r\n";
+			stream.write(("Content-Length: " + String.valueOf(file.length()) + "\r\n\r\n").getBytes());
+//			responseWriter.write(contentLength);
+//			responseWriter.write("\r\n\r\n");
+//			saida.writeLong((long) file.length());
+//			saida.flush();
+
+			// long i = 0;
+			
+			long tam = file.length();
+			int valor = 0;
+			byte[] contents;
+			int i = 0;
+			while (tam > 0) {
+
+				if (tam >= 1) {
+					tam = tam - 1;
+					valor = 1;
+				} else if (tam < 1) {
+					valor = (int) tam;
+					tam = 0;
+				}
+
+				contents = new byte[valor];
+				bis.read(contents, 0, valor);
+				stream.write(contents);
+				
+//				os.write(contents);
+//				os.flush();C
+
+				//System.out.println("Enviando arquivo ... " + (i * 100) /
+				//file.length() + "% completo!");
+				//i += 1;
+
+			}
+			stream.flush();		
+			fis.close();
+			bis.close();
+
+		} else {
+			responseWriter.write(OUTPUT_HEADERS_NOT_FOUND + OUTPUT_NOT_FOUND.length() + OUTPUT_END_OF_HEADERS + OUTPUT_NOT_FOUND);
+		}
+
+	}
+}
